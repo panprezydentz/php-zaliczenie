47f51853d1bc1cd6970da59a47b2ca61f9f6e4d5
Agnieszka Roksz <5009238@teb-gdansk.ad>
2024-05-11 11:09:27 +0200

    update pages

--- src/Controller.php
+++ src/Controller.php
@@ -33,22 +33,20 @@ class Controller
         switch ($this->action()) {
             case 'create':
                 $page = 'create';
-                $created = false;
                 $data = $this->getRequestPost();
                 if (!empty($data)) {
-                    $viewParams = [
+                    $noteData = [
                         'title' => $data['title'],
                         'description' => $data['description'],
                     ];
-                    // header('Location: /');
-                    $created = true;
-                    $this->database->createNote($viewParams);
+                    $this->database->createNote($noteData);
+                    header('Location: /?before=created');
                 }
-                $viewParams['created'] = $created;
                 break;
             default:
                 $page = 'list';
-                $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+                $data = $this->getRequestGet();
+                $viewParams['before']= $data['before'] ?? null;
         }
 
         $this->view->render($page, $viewParams);
--- templates/pages/list.php
+++ templates/pages/list.php
@@ -1,3 +1,19 @@
-<b><?php echo $params['resultList'] ?? "" ?></b>
+<div>
+    <div class="message">
+        <?php
+        if (!empty($params['before'])) {
+            switch ($params['before']) {
+                case 'created':
+                    echo "Notatka została utworzona!";
+                    break;
+                    default:
+                    echo "Błędny adres URL!";
+                    break;
+            }
+        }
+        ?>
+        </div>
+        <b><?php echo $params['resultList'] ?? "" ?></b>
 
-<h3>Lista notatek</h3>
+        <h3>Lista notatek</h3>
+    </div>
