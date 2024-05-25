c088225d75f667ebf64a463789d914f4dfb9e435
Agnieszka Roksz <5009238@teb-gdansk.ad>
2024-05-25 09:12:22 +0200

    view fixed

--- src/View.php
+++ src/View.php
@@ -6,17 +6,17 @@ namespace App;
 
 class View
 {
-    public function render(?string $page, array $params = []): void 
+    public function render(?string $page, array $params = []): void
     {
         $params = $this->escape($params);
         require_once('./templates/layout.php');
     }
-    private function escape (array $parrams): array 
+    private function escape(array $params): array
     {
         $clearParams = [];
 
         foreach ($params as $key => $param) {
-            if (is_array($parram)) {
+            if (is_array($param)) {
                 $clearParams[$key] = $this->escape($param);
             } else if ($param) {
                 $clearParams[$key] = htmlentities($param);
